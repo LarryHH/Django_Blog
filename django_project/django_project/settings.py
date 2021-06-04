0ed63967480c728ccc46f63a97137e7aa5197e79
Larry <21516059@student.uwa.edu.au>
2021-06-04 18:43:30 +0800

    added basic blog app with routing

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--&*&gc%nm1x79b)fsj+#krd9*)&lqe-z*0gr^kp)5vg#x555j2'
+SECRET_KEY = 'django-insecure--&*&gc%nm1x79b)fsj+#krd9*)&lqe-z*0gr^kp)5vg#x555j2' # lmfao
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')), #when someone goes to this route (''), send them to 'blogs.urls' and match path to any urlpatterns found in the remaining url path
 ]
