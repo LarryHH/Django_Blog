48020229bbb198c12dadc56fa6147b2b5c717ef4
Larry <21516059@student.uwa.edu.au>
2021-06-04 18:54:01 +0800

    added blog html templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(requests):
-    return HttpResponse('<h1>Blog Home</h1>')
+def home(request):
+    return render(request, 'blog/home.html')
 
-def about(requests):
-    return HttpResponse('<h1>Blog About</h1>')
+def about(request):
+    return render(request, 'blog/about.html')
