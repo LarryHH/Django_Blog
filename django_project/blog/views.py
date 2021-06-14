8a02766395a9031a49c84efab405fd95c7ae0858
Larry <larryhieuhuynh@gmail.com>
2021-06-15 01:10:14 +0800

    Part 3 - Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'LarryHH',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 15 2021'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 16 2021'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
